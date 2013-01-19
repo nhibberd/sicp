316f2d6843a53819916bb28c3ae934e12fc2cab1
nhibberd <nhibberd@gmail.com>
2013-01-20 02:34:02 +1000

    sqrt

--- src/main/scala/sicp/chapterone.scala
+++ src/main/scala/sicp/chapterone.scala
@@ -11,6 +11,8 @@ object One {
     println("abs(-5): " + abs(-5))
     println("2 >= 3: " + >=(4,3))
 
+    println("asd: " + sqrt(9))
+
     println("Exercises")
     println("1.3: " + onePointThree(2,3,4))
   }
@@ -33,21 +35,30 @@ object One {
 
   def >=(a: Int, b: Int): Boolean = a >= b
 
-  // Exercises
+  // ---------------- Exercises ----------------
 
-  // 1.3 - Define a Procedure that takes three numbers as arguments and returns the
-  //       sum  of the squares of the two larger numbers
+  // 1.3 - Define a Procedure that takes three numbers as arguments and returns
+  //       the sum  of the squares of the two larger numbers
   def onePointThree(a: Int, b: Int, c: Int): Int =  {
     var (x,y) = if (a > b && b > c) (a,b)
                   else if (a > b && c > b) (a,c)
                   else (b,c)
-    sumOfSquares(x,y)
-    
+    sumOfSquares(x,y)    
   }
+
+  // Square Roots by Newtons Method
+  def average(x: Int, y: Int) = (x + y) / 2
+  def improve(guess: Int, x: Int) = average(guess,(x / guess))
+  def goodEnough(guess: Int, x: Int): Boolean = abs(square(guess) - x) < 0.001
+  def sqrtIter(guess: Int, x: Int): Int = {
+    if (goodEnough(guess,x)) guess
+    else sqrtIter(improve(guess,x),x)
+  }
+  def sqrt(x: Int) = sqrtIter(1, x)
 
   // 1.x - x
   def x() = {
-    error("todo")
+    error("")
   }
 
 }
