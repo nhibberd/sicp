a34dd12624ea7b8e273b06aee2df8f3b82454f78
nhibberd <nhibberd@gmail.com>
2013-01-31 11:27:46 +1000

    update

--- src/main/scala/sicp/chapterone.scala
+++ src/main/scala/sicp/chapterone.scala
@@ -16,18 +16,6 @@ object One {
     println("factorialI: " + factorialI(6))
     println("fib(9): " + fib(9))
     println("fibb(9): " + fibb(9))
-
-    println(" ---- Exercises ---- ")
-    println("1.3 : " + onePointThree(2,3,4))
-    println("1.10 : " + A(0,0))
-    println("1.10 : " + A(0,1))
-    println("1.10 : " + A(0,2))
-    println("1.10 : " + A(0,3))
-    println("1.10 : " + A(0,4))
-    println("1.11 (R): " + fR(4))
-    println("1.11 (I): " + fI(4))
-    println("1.12 :  " + pascal(3,5))
-    println("1.12 : " + pascalrow(5))
   }
 
   // def <name>(<formal parramaters>): <return type> = <body>
@@ -99,58 +87,14 @@ object One {
     fibIter(1,0,i)
   }
 
-  // ------------------------------ Exercises ------------------------------
-
-  // 1.3 - Define a Procedure that takes three numbers as arguments and returns
-  //       the sum  of the squares of the two larger numbers
-  def onePointThree(a: Int, b: Int, c: Int): Int =  {
-    var (x,y) = if (a > b && b > c) (a,b)
-                  else if (a > b && c > b) (a,c)
-                  else (b,c)
-    sumOfSquares(x,y)    
+  def fastExpt(b: Int, n: Int): Int = {
+     val even  =  ( x :Int )  =>  x % 2 == 0
+     if (n==0) 1 
+     else if(even(n)) square(fastExpt(b,(n/2))) 
+     else (b * fastExpt(b,(n - 1)))
   }
 
-  // 1.10 - Ackermann's Function
-  def A(x: Int, y: Int): Int = (x,y) match {
-    case (0,_) => (y + 1)
-    case (a,0) if (a > 0) => A((x-1),1)
-    case (a,b) if ((a > 0) && (b > 0)) => A((x-1),A(x,(y-1)))
-  }
 
-  // 1.11 - Write procedures that computes f as an recursive / iterative process
-  //        f(n) = n                               n < 3
-  //        f(n) = f(n-1) + 2f(n-2) + 3f(n-3)      n >= 3
 
-  // recrusive
-  def fR(n: Int): Int = {
-    if (n < 3) n
-    else ( f(n-1) + (2 * f(n-2)) + (3 * f(n-3)) )
-  }
 
-  // iterative
-  def fI(n: Int): Int = {
-    def iter(count: Int, product: Int): Int = {
-      if (count > 3) product
-      else iter((count + 1), product + (count * f(n-count)))
-    }    
-    if (n < 3) n else iter(1,0)
-  }
-
-  // 1.12 - Write a procedure that computes elements of a Pascal's triangle by
-  //        means of a recursive process
-
-  def pascal(column: Int, row: Int): Int = {
-    if (column == 1 || column == row) 1
-    else pascal(column - 1, row - 1) + pascal(column, row - 1)
-  }
-
-  def pascalrow(i: Int): String = {
-    var r = "row " + i + ": "
-    for (x <- 1 to i) {
-      r = r + pascal(x,i) + " "
-    }
-    r
-  }
-
-  //
 }
